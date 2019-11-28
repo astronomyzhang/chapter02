378f55f194f4fd335395b4919cb2728c68b08850
Garwen <tianwen.astronomy.zhang@gmail.com>
2019-11-28 11:42:11 +0800

    delete idea, add some utils

---
+++ src/main/java/org/smart4j/chapter02/helper/DatabaseHelper.java
@@ -0,0 +1,14 @@
+package org.smart4j.chapter02.helper;/**
+ * @Author HP
+ * @Date 2019-11-28 11:05
+ * @Version 1.0
+ */
+
+/**
+ * Class Function Description
+ *
+ * @Author HP
+ * @Date 2019-11-28
+ */
+public class DatabaseHelper {
+}
